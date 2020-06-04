a62da9cad18e89b71a78e73bda2fc86963fe64ea
Renato Guimaraes Nunes <renatognunes@hotmail.com>
2020-06-03 22:56:16 -0300

    test

---
+++ client/.cache/sync-requires.js
@@ -0,0 +1,21 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/about.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/contact.js"))),
+  "component---src-pages-faq-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/faq.js"))),
+  "component---src-pages-gallery-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/gallery.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/index.js"))),
+  "component---src-pages-intellectual-property-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/intellectual-property.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/page-2.js"))),
+  "component---src-pages-partners-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/partners.js"))),
+  "component---src-pages-privacy-policy-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/privacy-policy.js"))),
+  "component---src-pages-services-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/services.js"))),
+  "component---src-pages-terms-js": hot(preferDefault(require("/Users/renatognunes/Documents/teampoker_project/client/src/pages/terms.js")))
+}
+
